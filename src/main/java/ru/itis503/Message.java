5d4f2f332c296c5806d41e97f1dd56aad7256539
magniklo <magniklom@gmail.com>
2025-11-29 09:07:42 +0300

    Добавлен класс Message в нем определены поля и конструктор. Добавлено 2 функции - поиск какой объект и вывод значения. Задача: дописать toString  к остальным

--- src/main/java/ru/itis503/Message.java
+++ src/main/java/ru/itis503/Message.java
@@ -1,4 +1,34 @@
 package ru.itis503;
 
 public class Message {
+    private User user; // от кого
+    private Group group;
+    private Channel channel;
+    private Object object; // кому
+    private String message;
+    private String time;
+
+    public Message(User user, Object object, String message, String time) {
+        this.user = user;
+        this.object = object;
+        this.message = message;
+        this.time = time;
+
+    }
+
+    private  String infoObject() {
+        if (object instanceof User) return ((User) object).getName();
+        else if (object  instanceof Group) return ((Group) object).getName();
+        else return ((Channel) object).getName();
+    }
+
+    public void aboutMessage(){
+        System.out.print("Отправлено сообщение { " +
+                "От - " + user.getName() +
+                ", кому - " + infoObject() +
+                ",сообщение - " + message + " }");
+        System.out.print("Дата отправления - " + time);
+    }
+
+
 }
