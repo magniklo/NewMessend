61d04d3ee9cb1307974eb3c34f889ecd6d361050
MatveevaElena72 <126193836+MatveevaElena72@users.noreply.github.com>
2025-11-29 11:21:52 +0500

    Создали класс Channel с конструктором и методом возвращающим его название

--- src/main/java/ru/itis503/Channel.java
+++ src/main/java/ru/itis503/Channel.java
@@ -1,4 +1,14 @@
 package ru.itis503;
 
 public class Channel {
+    private String name;
+
+    public Channel(String name){
+        this.name = name;
+    }
+
+    public String getName(String name){
+        return name;
+    }
+
 }
