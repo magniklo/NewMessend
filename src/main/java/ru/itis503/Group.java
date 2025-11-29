6d9a6e155fe0584fb96373dbac81935da0c9cb93
MatveevaElena72 <126193836+MatveevaElena72@users.noreply.github.com>
2025-11-29 11:20:50 +0500

    Создали класс Group  с конструктором и методом который возвращает его название

--- src/main/java/ru/itis503/Group.java
+++ src/main/java/ru/itis503/Group.java
@@ -1,4 +1,13 @@
 package ru.itis503;
 
 public class Group {
+    private String name;
+
+    public Group(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
 }
+
