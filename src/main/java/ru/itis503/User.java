7b90ab50bf928455d4c902045099cc22580c0e2f
magniklo <magniklom@gmail.com>
2025-11-29 09:04:45 +0300

    В классе User определено поле и добавлены конструктор и геттеры. Метод aboutUser возвращает информацию об участнике мессенджера

--- src/main/java/ru/itis503/User.java
+++ src/main/java/ru/itis503/User.java
@@ -1,4 +1,24 @@
 package ru.itis503;
 
 public class User {
+    private String name;
+    private String state;
+
+    public User(String name, String state) {
+        this.name = name;
+        this.state = state;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public void aboutUser() {
+        System.out.println(name + "   " + state);
+    }
+
 }
